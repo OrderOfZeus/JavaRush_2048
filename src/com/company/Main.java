9a1ce5bafa539c475f292fe5206ded54babf6ed8
OrderOfZeus <tigraneants@gmail.com>
2021-09-30 13:06:02 +0300

    Initial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,21 @@ package com.javarush.games.game2048;
 
 import com.javarush.engine.cell.*;
 
-public void DELETE TEST;
-
 public class Game2048 extends Game {
     private static final int SIDE = 4;
+    private int[][] gameField = new int[SIDE][SIDE];
     public void initialize(){
         setScreenSize(SIDE, SIDE);
+        createGame();
+        drawScene();
     }
+    private void createGame() {}
+    private void drawScene(){
+        for (int x=0; x < SIDE; x++){
+            for (int y=0; y<SIDE; y++){
+                setCellColor(x,y,Color.BLUE);
+            }
+        }
+    }
+
 }
